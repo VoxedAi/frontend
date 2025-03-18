fe1abae44f63bc3e19a5c11c76fb033735b1bc2b
Aidan Andrews <aidansa2@illinois.edu>
2025-03-18 09:31:46 -0500

    macos is really bugging recently | duplicate files

--- old_code/components 2/index.ts
+++
@@ -1,8 +0,0 @@
-// Export Sandbox components
-export { default as Sandbox } from './Sandbox';
-
-// Re-export components
-export { default as ChatInterface } from './ChatInterface';
-export { default as DeleteConfirmationModal } from './DeleteConfirmationModal';
-export { default as LoadingSpinner } from './LoadingSpinner';
-export { default as Sidebar } from './Sidebar'; 
